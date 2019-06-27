ee0e46831d368116f49ed3d81e2174356fa96385
dendrofen <ma9k97dev@gmail.com>
2019-06-27 13:48:48 +0300

    Changes

--- wp-content/themes/sales-shop-2019/footer.php
+++ wp-content/themes/sales-shop-2019/footer.php
@@ -1,5 +1,6 @@
 <?php
 echo do_shortcode('[wcs_switcher]');
+echo do_shortcode('[wcas-search-form]');
 wp_footer();
 ?>
 </body>
--- wp-content/themes/sales-shop-2019/functions.php
+++ wp-content/themes/sales-shop-2019/functions.php
@@ -1,25 +1,30 @@
 <?php
-define('SS_INC', get_template_directory().'/includes');
-define('SS_POST_TYPES', SS_INC.'/post_types');
-define('SS_POST_TYPE_FIELDS', SS_INC.'/post_type_fields');
+define('SS_INC', get_template_directory() . '/includes');
+define('SS_POST_TYPES', SS_INC . '/post_types');
+define('SS_POST_TYPE_FIELDS', SS_INC . '/post_type_fields');
+define('SS_CLASSES', SS_INC . '/classes');
 
 
 //theme options
-require_once (dirname(__FILE__) . '/redux-theme-config.php');
+require_once(dirname(__FILE__) . '/redux-theme-config.php');
 
 //menus
-require_once(SS_INC.'/menu.php');
+require_once(SS_INC . '/menu.php');
 
 
-//massive include
-$to_include = array(SS_POST_TYPES, SS_POST_TYPE_FIELDS);
-$except = array('.','..');
+//post types, post type fields, classes include
+$except = array('.', '..');
+$to_include = array(
+    SS_POST_TYPES,
+    SS_POST_TYPE_FIELDS,
+    SS_CLASSES
+);
 
-foreach($to_include as $one){
+foreach ($to_include as $one) {
     $files = scandir($one);
-    foreach($files as $item){
-        if( !in_array($item, $except) ){
-            require_once ($one.'/'.$item);
-        }  
+    foreach ($files as $item) {
+        if (!in_array($item, $except)) {
+            require_once($one . '/' . $item);
+        }
     }
 }
