0f04714b4ccb97b9f91cf80e76cf3f1b05b0649c
dendrofen <ma9k97dev@gmail.com>
2019-06-27 14:10:06 +0300

    Breadcrumbs changes

--- wp-content/plugins/dbc-breadcrumbs/includes/custom.php
+++ wp-content/plugins/dbc-breadcrumbs/includes/custom.php
@@ -1,11 +1,12 @@
 <?php
 //render items to code
-function dbc_render($container, $one, $last, $items){
+function dbc_render($container, $one, $last, $items)
+{
     $last_item = array_pop($items);
     $items_r = '';
 
     //all items, except last
-    foreach($items as $item){
+    foreach ($items as $item) {
         $item_one__r = str_replace('@name', $item['name'], $one);
         $item_one__r = str_replace('@link', $item['link'], $item_one__r);
         $items_r .= $item_one__r;
@@ -20,25 +21,28 @@ function dbc_render($container, $one, $last, $items){
 }
 
 //construct item
-function dbc_item($name, $link = null){
+function dbc_item($name, $link = null)
+{
     return compact('name', 'link');
 }
 
 //get breadcrumbs items
-function dbc_get_items(){
+function dbc_get_items()
+{
     $object = get_queried_object();
-    $items[] = dbc_item(_('Home'), get_home_url());    
+    $items[] = dbc_item(_('Home'), get_home_url());
 
-    if(is_single()){
-        $category = is_product() ? wp_get_post_terms($object->ID, 'product_cat') : get_the_category();
-        $term = array_shift($category);
-        if($term){
-            $items[] = dbc_item($term->name, get_term_link($term->term_id));
+    if ($_SERVER['REQUEST_URI'] !== '/') {
+        if (is_single()) {
+            $category = is_product() ? wp_get_post_terms($object->ID, 'product_cat') : get_the_category();
+            $term = array_shift($category);
+            if ($term) {
+                $items[] = dbc_item($term->name, get_term_link($term->term_id));
+            }
         }
+
+        $current = isset($object->name) ? $object->name : $object->post_title;
+        $items[] = dbc_item($current);
     }
-
-    $current = isset($object->name) ? $object->name : $object->post_title;
-    $items[] = dbc_item($current);
-
     return $items;
 }
