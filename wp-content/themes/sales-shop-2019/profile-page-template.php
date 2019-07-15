ccbe9f6ace1a94b76013dd03e94a5be50b4d2eba
dendrofen <ma9k97dev@gmail.com>
2019-07-15 13:40:32 +0300

    Updates to code

--- wp-content/themes/sales-shop-2019/profile-page-template.php
+++ wp-content/themes/sales-shop-2019/profile-page-template.php
@@ -3,15 +3,14 @@ get_header();
 
 $user = ss_get_user();
 
-if ($user->exists()) {
+if ($user) {
     if ($user->is_seller) {
         get_template_part('parts/profile', 'seller');
     } else {
         get_template_part('parts/profile', 'customer');
     }
 } else {
-    echo __('Please login first');
+    ss_return_home();
 }
 
 get_footer();
-?>
