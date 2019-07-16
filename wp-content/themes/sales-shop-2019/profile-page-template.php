c4c96b695206bff481b1f5e161d6779483962004
dmitry_krugovoy <dimonzmey@gmail.com>
2019-07-16 10:35:41 +0300

    Seller Dashboard

--- wp-content/themes/sales-shop-2019/profile-page-template.php
+++ wp-content/themes/sales-shop-2019/profile-page-template.php
@@ -1,16 +1,14 @@
 <?php /* Template Name: Profile Page Template */
-get_header();
-
 $user = ss_get_user();
 
 if ($user) {
     if ($user->is_seller) {
         get_template_part('parts/profile', 'seller');
-    } else {
+    } else if ($user->is_customer) {
         get_template_part('parts/profile', 'customer');
+    } else {
+        ss_return_home();
     }
 } else {
     ss_return_home();
 }
-
-get_footer();
