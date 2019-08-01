a9d07aac2a10815f125e9fe3ef079bc8f5247dad
dendrofen <ma9k97dev@gmail.com>
2019-08-01 16:20:07 +0300

    Content page template full

--- wp-content/themes/sales-shop-2019/content-page-template.php
+++ wp-content/themes/sales-shop-2019/content-page-template.php
@@ -1,112 +1,33 @@
-<?php /* Template Name: Content Page Template */ ?>
-<?php global $ss_theme_option; ?>
-
-<?php get_header() ?>
+<?php /* Template Name: Content Page Template */
+global $ss_theme_option;
+get_header() ?>
 <div class="content page">
 
     <?php ss_banner_full(get_field('banner')) ?>
 
     <div class="container">
-
         <?= do_shortcode('[dbc_breadcrumbs]'); ?>
-
         <h1 class="main-title"><?php the_title() ?></h1>
-
         <div class="content__content">
-            <div class="content__block">
-                <div class="row">
-
-                    <div class="col-md-4 content__block__photo order-md-2">
-                        <img src="img/card-big.jpg" alt="">
+            <?php foreach (get_field('blocks') as $one) : $image = $one['image'] ?>
+                <div class="content__block">
+                    <div class="row">
+                        <div class="col-md-4 content__block__photo <?= $image['position'] == 'right' ? 'order-md-2' : '' ?>">
+                            <?= wp_get_attachment_image($image['file']) ?>
+                        </div>
+                        <div class="col-md-8 content__block__text <?= $image['position'] == 'right' ? 'order-md-1' : '' ?>">
+                            <?php foreach ($one['text_blocks'] as $one_block) : ?>
+                                <?php if ($one_block['acf_fc_layout'] == 'text') : ?>
+                                    <p class="text"><?= $one_block['text'] ?></p>
+                                <?php elseif ($one_block['acf_fc_layout'] == 'subtitle') : ?>
+                                    <h2 class="content__subtitle"><?= $one_block['text'] ?></h2>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
-
-                    <div class="col-md-8 content__block__text order-md-1">
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum </p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur</p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum </p>
-
-                        <h2 class="content__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum</h2>
-
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur</p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum </p>
-
-                    </div>
-
                 </div>
-
-            </div>
-
-            <div class="content__block">
-                <div class="row">
-
-                    <div class="col-md-4 content__block__photo"><img src="img/card-big.jpg" alt=""></div>
-
-                    <div class="col-md-8 content__block__text">
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum </p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur</p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
-                            adipisicing lorem ipsum dolor sit amet, consectetur
-                            adipisicing ipsum dolor sit amet, tetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur
-                            adipisicing Lorem ipsum dolor sit amet, consectetur adipisicing lorem ipsum </p>
-                    </div>
-
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
-
     </div>
-
 </div>
 <?php get_footer() ?>
