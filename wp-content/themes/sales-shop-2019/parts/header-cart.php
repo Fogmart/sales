8dd63ce363922721c1dc7b07ec1f4cb8765de70b
Dmitry Krugovoy <dimonzmey@gmail.com>
2019-08-13 10:54:15 +0300

    Cart style

--- wp-content/themes/sales-shop-2019/parts/header-cart.php
+++ wp-content/themes/sales-shop-2019/parts/header-cart.php
@@ -1,12 +1,16 @@
 <?php
 $items = WC()->cart->get_cart();
+$cart_count = WC()->cart->get_cart_contents_count();
 ?>
 <span class="control__icon">
     <img src="<?= ss_asset('img/icons/cart.svg') ?>" alt="">
+    <?php if ($cart_count > 0) : ?>
     <span class="control__counter"><?= WC()->cart->get_cart_contents_count() ?></span>
+    <?php endif; ?>
 </span>
 <span class="control__link"><?= __('Cart') ?></span>
 <div class="control__cart control__popup">
+    <?php if (count($items) > 0) : ?>
     <div class="control__cart__content">
         <?php foreach ($items as $key => $one) : ?>
             <?php $product = apply_filters( 'woocommerce_cart_item_product', $one['data'], $one, $key ) //ss_get_product($one['product_id']) ?>
@@ -28,21 +32,22 @@ $items = WC()->cart->get_cart();
             </div>
         <?php endforeach; ?>
     </div>
-    <?php if (count($items) === 0) : ?>
-        <div><?= __('Your cart is empty') ?></div>
+    <div class="control__cart__total">
+        <div class="text"><?= __('Total') ?></div>
+        <div class="sum"><?= WC()->cart->total ?><?= get_woocommerce_currency_symbol() ?></div>
+    </div>
+    <div class="control__cart__buttons">
+        <a href="<?= wc_get_cart_url() ?>">
+            <button class="button button-1 button-1_120 button_light"><?= __('View cart') ?></button>
+        </a>
+        <a href="<?= wc_get_checkout_url() ?>">
+            <button class="button button-1 button-1_120"><?= __('Checkout') ?></button>
+        </a>
+    </div>
     <?php endif; ?>
-    <?php if (count($items) > 0) : ?>
-        <div class="control__cart__total">
-            <div class="text"><?= __('Total') ?></div>
-            <div class="sum"><?= WC()->cart->total ?><?= get_woocommerce_currency_symbol() ?></div>
-        </div>
-        <div class="control__cart__buttons">
-            <a href="<?= wc_get_cart_url() ?>">
-                <button class="button button-1 button-1_120 button_light"><?= __('View cart') ?></button>
-            </a>
-            <a href="<?= wc_get_checkout_url() ?>">
-                <button class="button button-1 button-1_120"><?= __('Checkout') ?></button>
-            </a>
+    <?php if (count($items) === 0) : ?>
+        <div class="control__cart__empty">
+            <?= __('Your cart is empty') ?>
         </div>
     <?php endif; ?>
 </div>
